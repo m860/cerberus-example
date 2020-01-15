ae83c39a51569f56c6ef2a9ffe1a2865cda49211
m860 <jean.ma.1986@gmail.com>
2020-01-15 18:09:19 +0800

    测试缓存

--- src/Routes.js
+++ src/Routes.js
@@ -5,9 +5,11 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from './screens/Home';
+import MemoryCache from './screens/MemoryCache';
 
 export default createAppContainer(
     createStackNavigator({
         Home,
+        MemoryCache,
     }),
 );
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,10 +3,10 @@
  * @author Jean.h.ma 2020/1/13
  */
 import * as React from 'react';
-import {ScrollView, SafeAreaView, Text, Button} from 'react-native';
+import {ScrollView, SafeAreaView, Text, Button, View} from 'react-native';
 import {useCerberus, useCloudCerberus} from '@m860/cerberus';
 
-export default function () {
+export default function ({navigation}) {
     const [, defined] = useCerberus({
         debug: true,
         entry: 'http://127.0.0.1:9000/hello.js',
@@ -28,11 +28,28 @@ export default function () {
         }
     }, [defined2]);
 
+
     return (
         <SafeAreaView>
             <ScrollView>
+                <Text style={{
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    backgroundColor: 'green',
+                    paddingVertical: 10,
+                    color: 'white',
+                }}>useCerberus</Text>
                 {renderHello()}
+                <Text style={{
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    backgroundColor: 'blue',
+                    paddingVertical: 10,
+                    color: 'white',
+                }}>useCloudCerberus</Text>
                 {renderCloudHello()}
+                <Button title="Memory Cache"
+                        onPress={() => navigation.navigate('MemoryCache')}/>
             </ScrollView>
         </SafeAreaView>
     );
