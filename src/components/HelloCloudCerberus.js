541d146cb55e87fd4e71b35c8f3c40c3d9e4843f
m860 <jean.ma.1986@gmail.com>
2020-01-15 11:12:15 +0800

    adjust

--- src/components/HelloCloudCerberus.js
+++ src/components/HelloCloudCerberus.js
@@ -3,7 +3,7 @@
  * @author Jean.h.ma 2020/1/13
  */
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 
 export default function () {
     return (
@@ -13,6 +13,11 @@ export default function () {
             backgroundColor: '#cfdf86',
         }}>
             <Text style={{fontWeight: 'bold', textAlign: 'center'}}>useCloudCerberus</Text>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Image source={require('../assets/cerberus.jpg')}
+                       resizeMode="contain"
+                       style={{width: 150, height: 150}}/>
+            </View>
             <Text style={{color: '#555555'}}>
                 我部署在Cloud Cerberus中
             </Text>
